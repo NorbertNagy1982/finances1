d9c29efbcb6d47980f4d0a611731cd2de3f4e9c0
NorbertNagy1982 <nagynorbert1@gmail.com>
2023-11-26 06:08:24 +0100

    test2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
